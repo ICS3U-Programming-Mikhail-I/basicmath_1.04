38f9c0ad49b0da3eb0ed4c4f4f0118563399d08b
Mikhail Ibrahim <Mikhail.ibrahim@stu.ocsb.ca>
2025-02-14 03:28:59 +0000

    Added Basic Math Operators program

---
+++ basicMath.cpp
@@ -0,0 +1,17 @@
+// Copyright (c) 2020 Mikhail Ibrahim All rights reserved.
+//
+// Created by Mikhail Ibrahim
+// Created on Feb 13, 2024.
+// This program does basic math
+
+#include <iostream>
+
+int main() {
+    std::cout << "Addition: " << 20 + 5 << std::endl;
+    std::cout << "Subtraction: " << 20 - 5 << std::endl;
+    std::cout << "Multiplication: " << 20 * 5 << std::endl;
+    std::cout << "Division: " << 20 / 5 << std::endl;
+    std::cout << "Modulo: " << 20 % 5 << std::endl;
+
+    return 0;
+}
